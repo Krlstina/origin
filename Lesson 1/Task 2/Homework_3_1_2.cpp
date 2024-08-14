45bd2c9a5b152195796076020e8afe0fd796d75f
Kristina Pankina <pankina_k00@mail.ru>
2024-08-14 23:16:11 +0300

    Add 2 and 3 tasks

---
+++ Lesson 1/Task 2/Homework_3_1_2.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+struct BankAccount
+{
+    int numberAccount{1};
+    std::string name{};
+    float balance{0.5f};
+};
+
+void changeBalance(BankAccount* account)
+{
+    std::cout << "Введите новый баланс счета: ";
+    std::cin >> account->balance;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    BankAccount person;
+
+    std::cout << "Введите номер счета: ";
+    std::cin >> person.numberAccount;
+    std::cout << "Введите имя владельца счета: ";
+    std::cin >> person.name;
+    std::cout << "Введите текущий баланс счета: ";
+    std::cin >> person.balance;
+
+    changeBalance(&person);
+
+    std::cout << "Ваш счет: " << person.numberAccount << ", " << person.name << ", " << person.balance;
+
+    return EXIT_SUCCESS;
+}
---
+++ Lesson 1/Task 3/Homework_3_1_3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+struct ResidentialAddress
+{
+    std::string city;
+    std::string street;
+    std::string houseNumber; //строковая переменная, там как номера домов бывают и с добавлением букв, например, "6А"
+    int apartmentNumber{1};
+    int index{1};
+};
+
+void structureOutput(ResidentialAddress* address)
+{
+    std::cout << "Город: " << address->city << std::endl;
+    std::cout << "Улица: " << address->street << std::endl;
+    std::cout << "Номер дома: " << address->houseNumber << std::endl;
+    std::cout << "Номер квартиры: " << address->apartmentNumber << std::endl;
+    std::cout << "Индекс: " << address->index << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ResidentialAddress address1 = { "Рязань", "Высоковольтная", "9В", 23, 390039 };
+    ResidentialAddress address2 = { "Москва", "8 марта", "12", 154, 117623 };
+
+    structureOutput(&address1);
+    std::cout << std::endl;
+    structureOutput(&address2);
+
+    return EXIT_SUCCESS;
+}
