4736cc26f72eab7eee8394952fb479b3542a0e07
Kristina Pankina <pankina_k00@mail.ru>
2024-08-26 23:04:50 +0300

    Adding Homework Lesson 3

---
+++ Lesson 3/Task 1/Homework_3_3_1.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <vector>
+
+class BlockAdress
+{
+private:
+    std::string city{};
+    std::string street{};
+    std::string houseNumber{};
+    std::string apartmentNumber{};
+
+public:
+    BlockAdress() {}
+
+   void getBlock(int numberRow)
+    {
+        std::string readLine{};
+        int readNumberBlocks{};
+        int currentLine{ 2 };
+        std::ifstream inFile("./in.txt");
+        inFile >> readNumberBlocks;
+        
+        while (currentLine < numberRow)
+        {
+            inFile >> readLine;
+            ++currentLine;
+        }
+        inFile >> readLine;
+        this->city = readLine;
+        inFile >> readLine;
+        this->street = readLine;
+        inFile >> readLine;
+        this->houseNumber = readLine;
+        inFile >> readLine;
+        this->apartmentNumber = readLine;
+
+        inFile.close();
+    }
+
+    void printAll()
+    {
+        std::ofstream outFile("./out.txt", std::ios::app);
+        outFile << this->city + ", " + this->street + ", " + this->houseNumber + ", " + this->apartmentNumber << std::endl;
+        outFile.close();
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::vector<BlockAdress*> blocks{};
+        
+    int numberBlocks;
+
+    std::ifstream inFile("./in.txt");
+    inFile >> numberBlocks;
+    inFile.close();
+
+    std::ofstream outFile("./out.txt", std::ios::app);
+    outFile << numberBlocks << std::endl;
+    outFile.close();
+
+    int counter{ 0 };
+    int numberRow{ ((numberBlocks * 4) - 2) };
+
+    while (counter < numberBlocks )
+    {
+        blocks.push_back(new BlockAdress{});
+        blocks[counter]->getBlock(numberRow);
+        blocks[counter]->printAll();
+        numberRow -= 4;
+        ++counter;
+    }
+
+    while (counter > 0) { delete blocks[counter-1];  --counter; }
+
+    return EXIT_SUCCESS;
+}
---
+++ Lesson 3/Task 2/Homework_3_3_2.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+class BlockAdress
+{
+private:
+    std::string city{};
+    std::string street{};
+    std::string houseNumber{};
+    std::string apartmentNumber{};
+
+public:
+    std::string getCity(){return city;};
+    BlockAdress() {}
+
+    void getBlock(int numberRow)
+    {
+        std::string readLine{};
+        int readNumberBlocks{};
+        int currentLine{ 2 };
+        std::ifstream inFile("./in.txt");
+        inFile >> readNumberBlocks;
+
+        while (currentLine < numberRow)
+        {
+            inFile >> readLine;
+            ++currentLine;
+        }
+        inFile >> readLine;
+        this->city = readLine;
+        inFile >> readLine;
+        this->street = readLine;
+        inFile >> readLine;
+        this->houseNumber = readLine;
+        inFile >> readLine;
+        this->apartmentNumber = readLine;
+
+        inFile.close();
+    }
+
+    void printAll()
+    {
+        std::ofstream outFile("./out.txt", std::ios::app);
+        outFile << this->city + ", " + this->street + ", " + this->houseNumber + ", " + this->apartmentNumber << std::endl;
+        outFile.close();
+    }
+
+    bool operator()(BlockAdress* adress1, BlockAdress* adress2) { return adress1->getCity() < adress2->getCity(); }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    BlockAdress compairObj;
+
+    std::vector<BlockAdress*> blocks{};
+
+    int numberBlocks;
+
+    std::ifstream inFile("./in.txt");
+    inFile >> numberBlocks;
+    inFile.close();
+
+    std::ofstream outFile("./out.txt", std::ios::app);
+    outFile << numberBlocks << std::endl;
+    outFile.close();
+
+    int counter{ 0 };
+    int numberRow{ 2 };
+
+    while (counter < numberBlocks)
+    {
+        blocks.push_back(new BlockAdress{});
+        blocks[counter]->getBlock(numberRow);
+        numberRow += 4;
+        ++counter;
+    }
+
+    std::sort(blocks.begin(), blocks.end(), compairObj);
+
+    counter = 0;
+
+    while (counter < numberBlocks)
+    {
+        blocks[counter]->printAll();
+        ++counter;
+    }
+
+    while (counter > 0) { delete blocks[counter - 1];  --counter; }
+
+    return EXIT_SUCCESS;
+}
