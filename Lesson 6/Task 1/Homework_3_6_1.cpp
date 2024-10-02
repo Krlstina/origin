8ff1dc62c36bc9f7810c8a1abf01b6d50645f7df
Kristina Pankina <pankina_k00@mail.ru>
2024-10-02 20:21:05 +0300

    Adding Homework Lesson 6

---
+++ Lesson 6/Task 1/Homework_3_6_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define MODE 1
+
+#ifdef MODE == 1
+void add(int num1, int num2)
+{
+    std::cout << "Результат сложения: " << (num1 + num2) << std::endl;
+}
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+#ifndef MODE
+    #error Не определено значение!!!
+#endif
+
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    int enterNumberFirst{};
+    int enterNumberSecond{};
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите первое число: ";
+    std::cin >> enterNumberFirst;
+    std::cout << "Введите второе число: ";
+    std::cin >> enterNumberSecond;
+    add(enterNumberFirst, enterNumberSecond);
+#else
+    #error Неизвестный режим. Завершение работы
+#endif
+
+    return EXIT_SUCCESS;
+}
---
+++ Lesson 6/Task 2/Homework_3_6_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#define SUB(numberFirst, numberSecond) ((numberFirst) - (numberSecond))
+
+int main()
+{
+    int numberFirst{ 6 };
+    int numberSecond{ 5 };
+    int numberThird{ 2 };
+
+    std::cout << SUB(numberFirst, numberSecond) << std::endl;
+    std::cout << SUB(numberFirst, numberSecond) * numberThird << std::endl;
+    std::cout << SUB(numberFirst, numberSecond + numberThird) * numberThird << std::endl;
+    
+    return EXIT_SUCCESS;
+}
