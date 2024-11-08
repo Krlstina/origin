3d88c0989854ab9708b4427c86fe9474ac841269
Kristina Pankina <pankina_k00@mail.ru>
2024-11-08 23:20:40 +0300

    Adding Homework ALGOCPP-24 Lesson 1

---
+++ ALGOCPP-24/Lesson 1/Task 2/Homework_4_1_2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <array>
+#include "Windows.h"
+
+int numberOfNumbersGreaterReferencePoint(int* arr, int referencePoint, int sizeArray)
+{
+    int left{};
+    int right{ sizeArray - 1 };
+    int middle{};
+    int numberGreaterReferencePoint{};
+
+    while (left < right)
+    {
+        middle = (left + right) / 2;
+        if (arr[middle] > referencePoint)
+        {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+
+    if (arr[left] > referencePoint)
+    {
+        numberGreaterReferencePoint = sizeArray - left;
+        return numberGreaterReferencePoint;
+    }
+    else {
+        numberGreaterReferencePoint = sizeArray - 1 - left;
+        return numberGreaterReferencePoint;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int sizeArray{ 9 };
+    int arr[sizeArray]{14, 16, 19, 32, 32, 32, 56, 69, 72};
+    int referencePoint{};
+
+    std::cout << "Введите точку отсчёта: ";
+    std::cin >> referencePoint;
+    std::cout << "Количество элементов в массиве больших, чем " << referencePoint << ": " << numberOfNumbersGreaterReferencePoint(arr, referencePoint, sizeArray);
+
+    return EXIT_SUCCESS;
+}
