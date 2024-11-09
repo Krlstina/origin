333c83ba9c5977bb58712bfcf61db00544544a3c
Kristina Pankina <pankina_k00@mail.ru>
2024-11-09 16:49:01 +0300

    Adding Homework ALGOCPP-24 Lesson 2

---
+++ ALGOCPP-24/Lesson 2/Task 1/Homework_4_2_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "Windows.h"
+
+unsigned long long int f(int number)
+{
+    if (number == 0) return 0;
+    if (number == 1 || number == 2) return 1;
+
+    return f(number - 1) + f(number - 2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int enterNumber{};
+    std::cout << "Введите порядковый номер числа Фибоначчи: ";
+    std::cin >> enterNumber;
+    std::cout << enterNumber << "-ое число Фибоначчи: " << f(enterNumber) << std::endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ ALGOCPP-24/Lesson 2/Task 2/Homework_4_2_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "Windows.h"
+
+int memory[10000];
+
+unsigned long long int f(int number)
+{
+    if (memory[number] == 0) {
+        if (number == 1 || number == 2) {
+            memory[number] = 1;
+        }
+        else {
+            memory[number] = f(number - 1) + f(number - 2);
+        }
+    }
+
+    return memory[number];
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int enterNumber{};
+    std::cout << "Введите порядковый номер числа Фибоначчи: ";
+    std::cin >> enterNumber;
+    std::cout << enterNumber << "-ое число Фибоначчи: " << f(enterNumber) << std::endl;
+
+    return EXIT_SUCCESS;
+}
